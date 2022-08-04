8e390c67915a07b5d9b69cd3d7e46152f801c9a6
Bruno Gusmão <brunogusmao43@gmail.com>
2022-08-04 16:08:25 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using exemploFundamentos_dotnet.Models;
+    // string apresentacao = "ola, bem vindo";
+    // int quantidade = 1;
+    // double altura = 1.80;
+    // decimal preco = 1.80M;
+
+    // Console.WriteLine(apresentacao);
+    // Console.WriteLine(quantidade);
+    // Console.WriteLine(preco);
+    // Console.WriteLine(altura);
+
+
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "BRUNO";
+//pessoa1.Idade = 19;
+//pessoa1.Apresentar();
+
+
+DateTime dataAtual = DateTime.Now.AddYears(1);
+Console.WriteLine(dataAtual);
+
+
+int a = 10;
+int b = 10;
+
+int c = a + 5;
+
+
