4ea35c485bd19271bb243d9295b0611d417dcdaf
Bruno Gusmão <brunogusmao43@gmail.com>
2022-08-31 18:03:54 -0300

    implementando metodos de retorno e remover

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using exemplosMetodosConstrutoresDotNet.Models;
 
+
+Pessoa p1 = new Pessoa();
+
+
+/*
 Pessoa p1 = new Pessoa();
 p1.Nome = "Bruno";
 p1.Sobrenome = "Mendonça Gusmão";
 p1.Idade = 19;
 p1.Apresentar();
-
+*/
 //TODO: Implementar metodos//
